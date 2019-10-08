5f13b477a416bd1c55aec8ad55d5e9996b35baa9
samthidny <andy.smith6@britishgas.co.uk>
2019-10-08 14:14:05 +0100

    add

--- script2.js
+++ script2.js
@@ -2,18 +2,40 @@ console.log('index 2');
 
 
 document.getElementById('meter-input').addEventListener('input', (event) => {
-    if (event.target.value.length > 4) {
-        event.target.value = event.target.value.slice(0,4); 
+    if (event.target.value.length > event.target.maxLength) {
+        event.target.value = event.target.value.slice(0, event.target.maxLength);
     }
 });
 
 
-document.getElementById('meter-input').addEventListener('input', (event) => {
-    var length = event.target.value.length;
-    var maxlength = event.target.maxLength;
+function fillPlaceholder(input, back) {
+    var length = input.value.length;
+    var maxlength = input.maxLength;
+    var zeros = 2;
     var str = '';
     for(var i = 0; i < maxlength; i++) {
-        str += i < length ? ' ' : '-';
+        str += i < zeros ? '0' : i < length ? ' ' : '-';
     }
-    document.getElementById('meter-input-back').value = str;
+
+    str += '.XXX';
+
+    back.value = str;
+}
+
+document.getElementById('meter-input').addEventListener('input', (event) => {
+    
+    fillPlaceholder(document.getElementById('meter-input'), document.getElementById('meter-input-back'));
+    
+    // var length = event.target.value.length;
+    // var maxlength = event.target.maxLength;
+    // var str = '';
+    // for(var i = 0; i < maxlength; i++) {
+    //     str += i < length ? ' ' : '-';
+    // }
+
+    // str += '.XXX';
+
+    // document.getElementById('meter-input-back').value = str;
 })
+
+fillPlaceholder(document.getElementById('meter-input'), document.getElementById('meter-input-back'));
