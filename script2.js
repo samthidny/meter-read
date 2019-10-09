3fef5a34717cb9d6bb1e741a0def75e349549206
samthidny <andy.smith6@britishgas.co.uk>
2019-10-09 11:18:13 +0100

    latest

--- script2.js
+++ script2.js
@@ -1,41 +1,68 @@
 console.log('index 2');
 
-
-document.getElementById('meter-input').addEventListener('input', (event) => {
-    if (event.target.value.length > event.target.maxLength) {
-        event.target.value = event.target.value.slice(0, event.target.maxLength);
-    }
-});
-
-
-function fillPlaceholder(input, back) {
+function fillPlaceholder(input, back, guide) {
     var length = input.value.length;
     var maxlength = input.maxLength;
-    var zeros = 2;
+    //var guide = '00----'
     var str = '';
     for(var i = 0; i < maxlength; i++) {
-        str += i < zeros ? '0' : i < length ? ' ' : '-';
+        str += i < length ? ' ' : guide.charAt(i);
     }
 
-    str += '.XXX';
+    if(back.getAttribute('post')) {
+        str += back.getAttribute('post');
+    }
 
     back.value = str;
 }
 
-document.getElementById('meter-input').addEventListener('input', (event) => {
+function setup(name, guide) {
+    // Restrict length
+    document.getElementById(name).addEventListener('input', (event) => {
+        
+        // Start template format test
+
+        var cursor = event.target.selectionStart;
+        var value = event.target.value;
+
+        var overlay = document.getElementById(`${name}-back`).getAttribute('overlay');
+
+        if (cursor === value.length && event.inputType !== 'deleteContentBackward' && overlay.charAt(cursor) !== ' ') {
+            console.log('Put a slash in now!');
+            event.target.value += overlay.charAt(cursor);
+            event.target.setSelectionRange(cursor + 1, cursor + 1);
+        }
+
+        //If you are on the last character and it is after a / then delte slash
+        if(cursor === value.length && event.inputType === 'deleteContentBackward' && overlay.charAt(cursor) !== ' ') {
+            console.log('You are deleting a template character!!!! ' + cursor + ' ' + overlay.charCodeAt(cursor - 1));
+            var arr = event.target.value.split('');
+            arr.pop();
+            event.target.value = arr.join('');
+        }
+
+
+        // If you have just deleted a slash and then you type, we need to add the slash back
+        if(cursor === value.length && event.inputType === 'insertText' && overlay.charAt(cursor - 1) !== ' ') {
+            var arr = event.target.value.split('');
+            arr.splice(arr.length - 1, 0, overlay.charAt(cursor - 1));
+            event.target.value = arr.join('');
+        }
+
+        // End template format test
+        
+        if (event.target.value.length > event.target.maxLength) {
+            event.target.value = event.target.value.slice(0, event.target.maxLength);
+        }
+    });
     
-    fillPlaceholder(document.getElementById('meter-input'), document.getElementById('meter-input-back'));
-    
-    // var length = event.target.value.length;
-    // var maxlength = event.target.maxLength;
-    // var str = '';
-    // for(var i = 0; i < maxlength; i++) {
-    //     str += i < length ? ' ' : '-';
-    // }
+    // Add input guide update listener
+    document.getElementById(name).addEventListener('input', (event) => {
+        fillPlaceholder(document.getElementById(name), document.getElementById(`${name}-back`), guide);
+    });
+    fillPlaceholder(document.getElementById(name), document.getElementById(`${name}-back`), guide);  
+}
 
-    // str += '.XXX';
-
-    // document.getElementById('meter-input-back').value = str;
-})
-
-fillPlaceholder(document.getElementById('meter-input'), document.getElementById('meter-input-back'));
+setup('meter-input', '00----');
+setup('dob-input', 'DD/MM/YYYY');
+setup('sort-input', '00-00-00');
